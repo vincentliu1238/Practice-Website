5b2b2c7e5c23e970407396bfa814d9369c8d9db8
root <root@Nova.localdomain>
2026-06-24 21:22:54 -0400

    Added an initial state for slot machine

--- Slot_Machine.js
+++ Slot_Machine.js
@@ -1,6 +1,6 @@
 function spinReels() {
     const numberOfReels = 5;
-    const display = document.getElementById('slot-images');
+    const display = document.querySelectorAll('.slot-images');
     const imageNames = ['7.png', 'banana.png', 'bar.png', 'bell.png', 'cherry.png', 'grape.png', 'lemon.png', 'orange.png', 'watermelon.png'];
     const values = [];
     const images = [];
@@ -9,7 +9,15 @@ function spinReels() {
         const randomIndex = Math.floor(Math.random() * 9) + 1;
         values.push(randomIndex);
         images.push(`<img src="Slot Machine Images/${imageNames[randomIndex - 1]}">`);
+    
+    display[i].innerHTML = images[i];
     }
-
-    display.innerHTML = images.join('');
 }
+
+window.onload = function() {
+    const displays = document.querySelectorAll('.slot-images');
+
+    for(let i = 0; i < displays.length; i++){
+        displays[i].innerHTML= '<img src="Slot Machine Images/question mark.png">';
+    }
+};
